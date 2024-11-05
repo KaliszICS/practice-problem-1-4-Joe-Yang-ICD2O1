6e93d2f906f81e585efb9c34f74be40ac794a71e
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-11-05 09:29:10 -0500

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -8,17 +8,25 @@ def q1():
 def q2():
   #Write Assignment code here
   fname = input("Input your first name: ")
+  print("Hello " + fname)
   
 
 def q3():
   #Write Assignment code here
+  fname = input("Input your first name: ")
+  lname = input("Input your last name: ")
+  print(f"{lname} {fname}")
 
 def q4():
   #Write Assignment code here
+  student1 =  input("Input a student: ")
+  student2 = input("Input another student: ")
+  print(f"Your students are {student1} and {student2}")
 
 #Do not edit code below this comment
-
+'''
 q1()
 q2()
 q3()
 q4()
+'''
